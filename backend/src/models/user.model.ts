36d39c06adebd873c5637d9f3a5487744841c30c
Aditya <adityabasak7031@gmail.com>
2025-02-10 11:53:13 +0530

    init user schema basic

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,1 +1,35 @@
-// model logic
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    DOB: {
+        type: Date,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+
+});
+
+export default mongoose.model('User', userSchema);
