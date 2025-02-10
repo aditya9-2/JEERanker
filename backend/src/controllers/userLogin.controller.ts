545e5f3612e1d02dd75cfa4796d60a4cea0a59c4
Aditya <adityabasak7031@gmail.com>
2025-02-10 12:47:04 +0530

    create user login API

--- backend/src/controllers/userLogin.controller.ts
+++ backend/src/controllers/userLogin.controller.ts
@@ -1,5 +1,44 @@
-const userLogin = async () => {
-    // Login Logic here
+import { Request, Response } from "express"
+import userModel from "../models/user.model";
+import jwt from "jsonwebtoken";
+
+const userLogin = async (req: Request, res: Response) => {
+
+    const { username, password } = req.body;
+    try {
+
+        if (!username || !password) {
+            res.status(400).json({
+                message: "All the fields are required",
+            });
+            return;
+        }
+
+        const user = await userModel.findOne({ username });
+
+        if (!user) {
+            res.status(404).json({
+                message: "user not found",
+            });
+            return;
+        }
+
+        const token = jwt.sign({
+            id: user._id
+        }, process.env.JWT_SECRET as string);
+
+        res.status(200).json({
+            message: "user Login successfull",
+            token
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error while login",
+
+        });
+        return;
+    }
 }
 
 export default userLogin
